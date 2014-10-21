c059f544bc1e09b2d43a35333f1b94585715104e
Zsolt Szabo <zsolt.szabo@liferay.com>
2014-10-21 19:29:00 +0200

    #1 Init Time Management portlet

---
+++ docroot/WEB-INF/src/com/liferay/timemanagement/task/portlet/TaskPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.timemanagement.task.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class TaskPortlet
+ */
+public class TaskPortlet extends MVCPortlet {
+ 
+
+}
