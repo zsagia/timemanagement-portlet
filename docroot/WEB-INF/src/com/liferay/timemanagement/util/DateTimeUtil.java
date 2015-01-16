256a41cfea36a295bd96fe4cff1d67d5ea2c3d5d
Zsolt Szabo <zsolt.szabo@liferay.com>
2015-01-16 22:50:40 +0100

    #19 New Util classes

---
+++ docroot/WEB-INF/src/com/liferay/timemanagement/util/DateTimeUtil.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.timemanagement.util;
+
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
+
+import java.util.Calendar;
+import java.util.Date;
+public class DateTimeUtil {
+
+	public static Date getDayWithoutTime(Date date) {
+		Calendar calendar = CalendarFactoryUtil.getCalendar();
+
+		calendar.setTime(date);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+
+		return calendar.getTime();
+	}
+
+	public static Date getIncrementedDay(Date day) {
+		Calendar calendar = CalendarFactoryUtil.getCalendar();
+
+		calendar.setTime(day);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		calendar.set(Calendar.MILLISECOND, 0);
+		calendar.add(Calendar.DATE, 1);
+
+		return calendar.getTime();
+	}
+
+}
